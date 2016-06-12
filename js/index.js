62bd895e391c754b3e6753b9c3c8f4d06580091f
iamropo <iamropo@gmail.com>
2016-06-12 17:04:35 +0530

    standard formatting

--- js/index.js
+++ js/index.js
@@ -1,109 +1,209 @@
-//Required Containers:
-var mediaLinksContainer = getElementById('media-links-container');
-var slideContainer = getElementById('slide-container');
-//Function for looping a collection: (Doesn't work, Fix it.)
-var loopCollection = Array.prototype.forEach.call;
+/* global slidesData */
+// Required Containers:
+var mediaLinksContainer = getElementById('media-links-container')
+var slideContainer = getElementById('slide-container')
 
-//DOM Functions:
-function getElementById(id) {
-	return document.getElementById(id);
+// DOM Functions:
+function getElementById (id) {
+  return document.getElementById(id)
 }
-function getElementsByClassName(className) {
-	return document.getElementsByClassName(className);
+function getElementsByClassName (className) {
+  return document.getElementsByClassName(className)
 }
-function createElement(tag) {
-	return document.createElement(tag);
-}
-function getAttribute(element, attribute) {
-	return element.getAttribute(attribute);
+function createElement (tag) {
+  return document.createElement(tag)
 }
 
-//Functions for HTML Collections
-function concatenetCollections(collections) {
+// Functions for HTML Collections
+function concatenetCollections (collections) {
   return Array.prototype.reduce.call(collections, function (prev, current) {
-    return Array.prototype.concat.call(prev, current);
-  });
+    return Array.prototype.concat.call(prev, current)
+  })
 }
 
-function loopCollection(collection, callback) {
-  return Array.prototype.forEach.call(collection, callback);
+function loopCollection (collection, callback) {
+  return Array.prototype.forEach.call(collection, callback)
 }
-//Function to retrieve the index of the active slide based on the active media link:
-function getActiveSlideIndex() {
-  var activeIndex;
+// Function to retrieve the index of the active slide based on the active media link:
+function getActiveSlideIndex () {
+  var activeIndex
   loopCollection(mediaLinks, function (link, index) {
     if (link.className.indexOf('on') !== -1) {
-       activeIndex = index;
+      activeIndex = index
     }
-  });
-  return activeIndex;
+  })
+  return activeIndex
 }
-//To toggle visibility of the buttons
-function toggleButton(index) {
+// To toggle visibility of the buttons
+function toggleButton (index) {
   if (index === 0) {
-    getElementById('prev-button').style.visibility = 'hidden';
-    getElementById('next-button').style.visibility = 'visible';
+    getElementById('prev-button').style.visibility = 'hidden'
+    getElementById('next-button').style.visibility = 'visible'
   } else if (index === 3) {
-    getElementById('prev-button').style.visibility = 'visible';
-    getElementById('next-button').style.visibility = 'hidden';
+    getElementById('prev-button').style.visibility = 'visible'
+    getElementById('next-button').style.visibility = 'hidden'
   } else {
-    getElementById('prev-button').style.visibility = 'visible';
-    getElementById('next-button').style.visibility = 'visible';
+    getElementById('prev-button').style.visibility = 'visible'
+    getElementById('next-button').style.visibility = 'visible'
   }
 }
-//Function for adding common Class Name:
-function setClassName(elements, classNames) {
-	var className = classNames.join(' ');
-	elements.forEach(function (element) {
-		element.className = className;
-	});
-}
-//Function for appending child element to parent element
-function appendToContainer(parent, childrens){
-	childrens.forEach(function (children) {
-		parent.appendChild(children);
-	});
+
+// Function for appending child element to parent element
+function appendToContainer (parent, childrens) {
+  childrens.forEach(function (children) {
+    parent.appendChild(children)
+  })
 }
 
-//Function for offsetting the Slides:
-function offsetElement(element, offsetPercentage, offsetFactor) {
-	var offsetValue = (offsetPercentage * offsetFactor).toString();
-	element.style.left = offsetValue + '%';
+// Function for offsetting the Slides:
+function offsetElement (element, offsetPercentage, offsetFactor) {
+  var offsetValue = (offsetPercentage * offsetFactor).toString()
+  element.style.left = offsetValue + '%'
 }
-//Rendering the Slides Data:
-function renderData(slidesData) {
-	//Offsett percentage value for the elements that are going to slide
-	var offsetPercentage = 100;
-	slidesData.forEach(function (slideData, index) {
+// Rendering the Slides Data:
+function renderData (slidesData) {
+  // Offsett percentage value for the elements that are going to slide
+  var offsetPercentage = 100
+  slidesData.forEach(function (slideData, index) {
+    var slide = createElement('div')
+    var header = createElement('h2')
+    var description = createElement('div')
+    var media = createElement('img')
+    var anchor = createElement('a')
+    var initialLinkState = 'off'
 
-		var slide = createElement('div');
-		var header = createElement('h2');
-		var description = createElement('div');
-		var media = createElement('img');
-		var anchor = createElement('a');
-		var initialLinkState = 'off'
+    slide.className = 'slide'
+    slide.id = 'slide-' + index
+    slide.style.position = 'absolute'
+    header.innerHTML = slideData.header
+    header.className = 'header'
+    description.innerHTML = slideData.description
+    description.className = 'description'
+    media.src = slideData.media
+    media.className = 'media'
+    anchor.className = 'media-link' + ' ' + initialLinkState
+    anchor.id = index
 
-		slide.className = 'slide';
-		slide.id = 'slide-' + index;
-		slide.style.position = 'absolute';
-		header.innerHTML = slideData.header;
-		header.className = 'header';
-		description.innerHTML = slideData.description;
-		description.className = 'description';
-		media.src = slideData.media;
-		media.className = 'media';
-		anchor.className = 'media-link' + ' ' + initialLinkState;
-		anchor.id = index;
+    var slideChildrens = [header, description, media]
 
-		var slideChildrens = [header, description, media];
-
-		mediaLinksContainer.appendChild(anchor);
-		appendToContainer(slide, slideChildrens);
-		offsetElement(slide, offsetPercentage, index);
-		slideContainer.appendChild(slide);
-
-	});
-
+    mediaLinksContainer.appendChild(anchor)
+    appendToContainer(slide, slideChildrens)
+    offsetElement(slide, offsetPercentage, index)
+    slideContainer.appendChild(slide)
+  })
 }
 
-renderData(slidesData);
+renderData(slidesData)
+
+/* Carousel.js */
+
+// Fetching the navigation buttons:
+var navigationButtons = getElementsByClassName('navigation-button')
+// Fetching the the slides:
+var slides = getElementsByClassName('slide')
+// Fetching the media links:
+var mediaLinks = getElementsByClassName('media-link')
+// Collection fo HTML elements that are qualified to stop the carousel:
+var qualifiedCollection = concatenetCollections([navigationButtons, slides, mediaLinks])
+
+// Function for Translating slides:
+function translateSlides (slides, slideOffset) {
+  Array.prototype.forEach.call(slides, function (slide) {
+    slide.style.transform = 'translateX(' + slideOffset.toString() + '%)'
+    slide.style.transition = 'transform 1s'
+  })
+}
+
+function startCarousel () {
+  var index = 0
+  // For the slides
+  var slideOffset = 0
+  var slideOffsetPercentage = 100
+  var currentSlideIndex
+
+  mediaLinks[index].className = mediaLinks[index].className.replace('off', 'on')
+  toggleButton(index)
+  function switchLink () {
+    var prevSlideIndex = currentSlideIndex || index
+    mediaLinks[index].className = mediaLinks[index].className.replace('on', 'off')
+    index++
+    if (index === mediaLinks.length) {
+      index = 0
+    }
+    currentSlideIndex = index
+    toggleButton(currentSlideIndex)
+    mediaLinks[index].className = mediaLinks[index].className.replace('off', 'on')
+    slideOffset = slideOffset + ((prevSlideIndex - currentSlideIndex) * slideOffsetPercentage)
+    translateSlides(slides, slideOffset)
+  }
+
+  var carouselTimer = setInterval(switchLink, 3000)
+
+  return carouselTimer
+}
+
+var initiateCarousel = startCarousel()
+// Stopping the Carousel:
+qualifiedCollection.forEach(function (collection) {
+  loopCollection(collection, function (element) {
+    element.addEventListener('mousedown', function () {
+      clearInterval(initiateCarousel)
+    })
+  })
+})
+
+/* Navigation.js */
+
+/* Note: The slide offset values are negative,
+ because the slides are offset relative to right offset which takes negative value to occur*/
+
+function mediaLinkNavigation (link, anchor) {
+  var activeSlideIndex = getActiveSlideIndex()
+  var requestedSlideIndex = Number(link.id)
+  toggleButton(requestedSlideIndex)
+  var slideOffsetPercentage = 100
+  var activeSlideOffset = - activeSlideIndex * slideOffsetPercentage
+  var slideOffset
+
+  if (requestedSlideIndex !== activeSlideIndex) {
+    mediaLinks[activeSlideIndex].className = mediaLinks[activeSlideIndex].className.replace('on', 'off')
+    mediaLinks[requestedSlideIndex].className = mediaLinks[requestedSlideIndex].className.replace('off', 'on')
+
+    slideOffset = activeSlideOffset + ((activeSlideIndex - requestedSlideIndex) * slideOffsetPercentage)
+    translateSlides(slides, slideOffset)
+  }
+}
+
+function buttonNavigation (direction) {
+  // True is to forward, False is to backwards
+  var slideOffset
+  // Boundary issue will be solved by hiding the button with css
+  var activeSlideIndex = getActiveSlideIndex()
+  var slideOffsetPercentage = 100
+  var currentOffset = activeSlideIndex * slideOffsetPercentage
+
+  if (direction) {
+    var nextSlideIndex = activeSlideIndex + 1
+  } else {
+    var prevSlideIndex = activeSlideIndex - 1
+  }
+  toggleButton(nextSlideIndex || prevSlideIndex)
+  // dealing with media links, also the Boundary issue will be solved by hiding the button with css
+  mediaLinks[activeSlideIndex].className = mediaLinks[activeSlideIndex].className.replace('on', 'off')
+  if (nextSlideIndex) {
+    slideOffset = (-(currentOffset)) - slideOffsetPercentage
+    mediaLinks[nextSlideIndex].className = mediaLinks[nextSlideIndex].className.replace('off', 'on')
+  } else {
+    slideOffset = (-(currentOffset)) + slideOffsetPercentage
+    mediaLinks[prevSlideIndex].className = mediaLinks[prevSlideIndex].className.replace('off', 'on')
+  }
+  translateSlides(slides, slideOffset)
+}
+
+getElementById('next-button').addEventListener('mousedown', function () {buttonNavigation(true)})
+getElementById('prev-button').addEventListener('mousedown', function () {buttonNavigation(false)})
+loopCollection(mediaLinks, function (mediaLink) {
+  mediaLink.addEventListener('mousedown', function () {
+    mediaLinkNavigation(this)
+  })
+})
--- js/slides-data.js
+++ js/slides-data.js
@@ -1,22 +1,22 @@
 var slidesData = [
-{
-	header: 'BLINK 182',
-	description: 'Enema Of The State.',
-	media: 'images/blink182.jpg'
-},
-{
-	header: 'MEGADETH',
-	description: 'Symphony Of Destruction.',
-	media: 'images/megadeth.jpg'
-},
-{
-	header: 'GUNS N ROSES',
-	description: 'The Most Dangerous Band In The World.',
-	media: 'images/guns-n-roses.jpg'
-},
-{
-	header: 'GREEN DAY',
-	description: 'American Idiots.',
-	media: 'images/green-day.jpg',
-}
-];
+  {
+    header: 'BLINK 182',
+    description: 'Enema Of The State.',
+    media: 'images/blink182.jpg'
+  },
+  {
+    header: 'MEGADETH',
+    description: 'Symphony Of Destruction.',
+    media: 'images/megadeth.jpg'
+  },
+  {
+    header: 'GUNS N ROSES',
+    description: 'The Most Dangerous Band In The World.',
+    media: 'images/guns-n-roses.jpg'
+  },
+  {
+    header: 'GREEN DAY',
+    description: 'American Idiots.',
+    media: 'images/green-day.jpg'
+  }
+]
